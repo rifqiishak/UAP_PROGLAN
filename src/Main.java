8eb36426eb26bfff9e592991a33895d7f4bd225a
nadif <nadifakiyama@gmail.com>
2024-12-28 00:05:13 +0700

    sad

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginGUI loginGUI = new LoginGUI();
+            loginGUI.setVisible(true);
+        });
+    }
+}
+
